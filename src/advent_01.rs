1a06d125c7d7d4f055c951d8d8497c51e9cd1e65
Pelle <pelle.mansson@attentec.se>
2020-12-03 10:08:47 +0100

    Fix Advent 01 a bit

--- src/advent_01.rs
+++ src/advent_01.rs
@@ -2,23 +2,23 @@ pub struct Advent;
 
 use super::common::parse_lines;
 
-fn find_indices(what: &Vec<i64>) -> Option<(usize, usize)> {
-    for i in 0..what.len() {
-        for j in i + 1..what.len() {
-            if what[i] + what[j] == 2020 {
-                return Some((i, j));
-            }
+fn find_values(what: &Vec<i64>) -> Option<(i64, i64)> {
+    let values = what.iter().collect::<std::collections::HashSet<_>>();
+    for x in what.iter() {
+        if values.contains(&(2020 - x)) {
+            return Some((*x, 2020 - x));
         }
     }
     None
 }
-fn find_indices2(what: &Vec<i64>) -> Option<(usize, usize, usize)> {
+
+fn find_values2(what: &Vec<i64>) -> Option<(i64, i64, i64)> {
+    let values = what.iter().collect::<std::collections::HashSet<_>>();
     for i in 0..what.len() {
-        for j in i + 1..what.len() {
-            for k in j + 1..what.len() {
-                if what[i] + what[j] + what[k] == 2020 {
-                    return Some((i, j, k));
-                }
+        let x = what[i];
+        for y in what[i + 1..].iter() {
+            if values.contains(&(2020 - x - y)) {
+                return Some((x, *y, 2020 - x - y));
             }
         }
     }
@@ -32,13 +32,13 @@ impl super::common::Advent for Advent {
 
     fn main1(input: &String) -> String {
         let what = parse_lines(input);
-        let (i, j) = find_indices(&what).unwrap();
-        format!("{}", what[i] * what[j])
+        let (x, y) = find_values(&what).unwrap();
+        format!("{}", x * y)
     }
 
     fn main2(input: &String) -> String {
         let what = parse_lines(input);
-        let (i, j, k) = find_indices2(&what).unwrap();
-        format!("{}", what[i] * what[j] * what[k])
+        let (x, y, z) = find_values2(&what).unwrap();
+        format!("{}", x * y * z)
     }
 }
